711663fb698728955f40f228d3c89373767cb14e
Prateek Aghi <prateekaghi42@gmail.com>
2022-12-07 16:57:42 +0530

    updates

--- ecom/pages/products/[id].vue
+++ ecom/pages/products/[id].vue
@@ -1,8 +1,6 @@
 <script setup>
 const { id } = useRoute().params;
 
-const url = "https://fakestoreapi.com/products/" + id;
-
 const { data: product } = await useFetch(`/api/singleProduct/${id}`);
 
 if (!product.value) {
